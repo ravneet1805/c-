11c1376d7188614d347b916a1abc356aae67260b
ravneet1805 <97474782+ravneet1805@users.noreply.github.com>
2022-11-01 15:48:00 +0530

    Add files via upload

---
+++ If:else/OddEven.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int n;
+
+  cin>>n;
+
+  if (n>=2){
+
+
+
+    if (n%2==0){
+      cout<<n <<" is even";
+
+    }
+    else{
+
+      cout<<n <<" is odd";
+    }
+
+
+  }
+
+
+  return 0;
+}
